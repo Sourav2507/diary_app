19d55c916a5c3e56b3b30402b78dbe06b129e600
Sourav2507 <132735088+Sourav2507@users.noreply.github.com>
2025-07-01 19:05:14 +0530

    New Changes in Firebase Authentication

--- app.py
+++ app.py
@@ -6,13 +6,9 @@ app = Flask(__name__)
 def landing():
     return render_template('landing.html')
 
-@app.route('/register')
-def register():
-    return render_template('register.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
+@app.route('/authenticate')
+def authenticate():
+    return render_template('authenticate.html')
 
 @app.route('/dashboard')
 def dashboard():
